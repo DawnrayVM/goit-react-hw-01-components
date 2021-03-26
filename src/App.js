756bcece6991eb94852cee32d85e44d7eac623ef
Valeriy Mashura <dr.mashura@gmail.com>
2021-03-26 18:54:13 +0200

    all hw done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import Profile from "./components/Profile.jsx";
-import Statistics from "./components/Statistics.jsx";
-import Friendlist from "./components/Friendlist.jsx";
-import user from "./user.json";
-import statistics from "./statistical-data.json";
-import friends from "./friends.json";
+import Profile from './components/Profile.jsx';
+import Statistics from './components/Statistics.jsx';
+import Friendlist from './components/Friendlist.jsx';
+import TransactionHistory from './components/TransactionHistory';
+import user from './user.json';
+import statistics from './statistical-data.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
 const App = () => (
   <div className="App">
     <Profile
       avatar={user.avatar}
       name={user.name}
-      tag={user.tag[0] === "@" ? user.tag.slice(1) : user.tag}
+      tag={user.tag[0] === '@' ? user.tag.slice(1) : user.tag}
       location={user.location}
       followers={user.stats.followers}
       views={user.stats.views}
@@ -21,6 +23,8 @@ const App = () => (
     <Statistics statistics={statistics} />
 
     <Friendlist friends={friends} />
+
+    <TransactionHistory items={transactions} />
   </div>
 );
 
